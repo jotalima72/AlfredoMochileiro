a2349b9ee60332d8d7a7aae031b4efae3dc00a97
Daniel <danielbb2000@gmail.com>
2022-09-30 17:46:30 -0300

    Feito o cruzamento

--- Models/Mochila.cs
+++ Models/Mochila.cs
@@ -5,51 +5,70 @@ using System.Threading.Tasks;
 
 namespace ALfredoMochileiro.Models
 {
-    public class Mochila
+  public class Mochila
+  {
+    private List<Item> Itens = new List<Item>();
+    public float Aptidations = 0;
+    private float CapacidadeMaxima;
+    private float Capacidade { get; set; }
+    private float PrecoTotal = 0;
+
+    //Construtor para a mochila pai
+    public Mochila(float capacidadeMaxima, List<Item> itensDisponiveis)
     {
-        private List<Item> Itens = new List<Item>();
-        public float Aptidations = 0;
-        private float CapacidadeMaxima;
-        private float Capacidade { get; set; }
-        private float PrecoTotal = 0;
+      this.CapacidadeMaxima = capacidadeMaxima;
+      var rand = new Random();
+      int numItens = rand.Next(3, itensDisponiveis.Count());
 
-        public Mochila(float capacidadeMaxima, List<Item> itensDisponiveis)
-        {
-            this.CapacidadeMaxima = capacidadeMaxima;
-            var rand = new Random();
-            //int numItens = rand.Next(itensDisponiveis.Count()/2,itensDisponiveis.Count());
-            int numItens = rand.Next(3,itensDisponiveis.Count());
-            
-            for (int i = 0; i < numItens; i++)
-            {
-                int id = rand.Next(itensDisponiveis.Count());
-                this.Itens.Add(itensDisponiveis[id]);
-                this.Capacidade += itensDisponiveis[id].Peso;
-                this.PrecoTotal += itensDisponiveis[id].Preco;
-                itensDisponiveis.Remove(itensDisponiveis[id]);
-            }
-            this.Aptidations = calcularAptidao();
-        }
-
-        private float calcularAptidao()
-        {
-            float aptidao = this.Capacidade;
-            aptidao += this.PrecoTotal * this.PrecoTotal;
-            if (this.Capacidade > this.CapacidadeMaxima) aptidao /= (this.Capacidade * this.Capacidade * this.Capacidade);
-            return aptidao;
-        }
-
-        public override string ToString()
-        {
-            string retorno = "";
-            foreach (Item item in Itens)
-            {
-                retorno += "|\t" + item.ToString() + "\n";
-            }
-            retorno += "|Capacidade: " + this.Capacidade + "\n";
-            retorno += "|Preço Total: " + this.PrecoTotal + "\n";
-            retorno += "|Aptidão: " + this.Aptidations + "\n";
-            return retorno;
-        }
+      for (int i = 0; i < numItens; i++)
+      {
+        int id = rand.Next(itensDisponiveis.Count());
+        this.Itens.Add(itensDisponiveis[id]);
+        this.Capacidade += itensDisponiveis[id].Peso;
+        this.PrecoTotal += itensDisponiveis[id].Preco;
+        itensDisponiveis.Remove(itensDisponiveis[id]);
+      }
+      this.Aptidations = calcularAptidao();
     }
+
+    //Construtor para a mochila filho
+    public Mochila(float capacidadeMaxima, List<Item> itens, bool isFilho)
+    {
+      this.CapacidadeMaxima = capacidadeMaxima;
+      for (int i = 0; i < itens.Count(); i++)
+      {
+        this.Itens.Add(itens[i]);
+        this.Capacidade += itens[i].Peso;
+        this.PrecoTotal += itens[i].Preco;
+      }
+      this.Aptidations = calcularAptidao();
+    }
+    
+    public List<Item> GetItems()
+    {
+        this.Itens.Sort((a,b)=> a.Nome.CompareTo(b.Nome));
+        return this.Itens;
+      
+    }
+    private float calcularAptidao()
+    {
+      float aptidao = this.Capacidade;
+      aptidao += this.PrecoTotal * this.PrecoTotal;
+      if (this.Capacidade > this.CapacidadeMaxima) aptidao /= (this.Capacidade * this.Capacidade * this.Capacidade);
+      return aptidao;
+    }
+
+    public override string ToString()
+    {
+      string retorno = "";
+      foreach (Item item in Itens)
+      {
+        retorno += "|\t" + item.ToString() + "\n";
+      }
+      retorno += "|Capacidade: " + this.Capacidade + "\n";
+      retorno += "|Preço Total: " + this.PrecoTotal + "\n";
+      retorno += "|Aptidão: " + this.Aptidations + "\n";
+      return retorno;
+    }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -4,118 +4,181 @@ using ALfredoMochileiro.Models;
 
 namespace ALfredoMochileiro
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            const float CapacidadeMaxima = 15;
-            const int NumMochilas = 10;
-            float aptidaoMaxima = 0;
-            Console.WriteLine("Hello Worlds");
-            List<Item> itensUsados = new List<Item>();
-            List<Mochila> populacao = new List<Mochila>();
-            itensUsados.Add(new Item(3, 5, "violao"));
-            itensUsados.Add(new Item(2, 2, "Capacete"));
-            itensUsados.Add(new Item(1, 6, "Relogio"));
-            itensUsados.Add(new Item(4, 10, "Computador"));
-            itensUsados.Add(new Item(3, 8, "Celular"));
-            itensUsados.Add(new Item(2, 7, "ventilador"));
-            itensUsados.Add(new Item(5, 2, "Arvore"));
-            itensUsados.Add(new Item(5, 1, "Pedra"));
-            itensUsados.Add(new Item(4, 4, "Macaco"));
-            itensUsados.Add(new Item(3, 6, "Moletom"));
+      const float CapacidadeMaxima = 15;
+      const int NumMochilas = 10;
+      float aptidaoMaxima = 0;
+      Console.WriteLine("Hello Worlds");
+      List<Item> itensDisponiveis = new List<Item>();
+      List<Mochila> populacao = new List<Mochila>();
 
-            for (int i = 0; i < NumMochilas; i++)
-            {
-                Mochila aux = new Mochila(CapacidadeMaxima, new List<Item>(itensUsados));
-                populacao.Add(aux);
-                aptidaoMaxima += aux.Aptidations;
-            }
+      //Metade 1
+      itensDisponiveis.Add(new Item(5, 2, "Arvore"));
+      itensDisponiveis.Add(new Item(2, 2, "Capacete"));
+      itensDisponiveis.Add(new Item(3, 8, "Celular"));
+      itensDisponiveis.Add(new Item(4, 10, "Computador"));
+      itensDisponiveis.Add(new Item(4, 4, "Macaco"));
 
-            populacao.Sort((a, b) => b.Aptidations.CompareTo(a.Aptidations));
-            for (int i = 0; i < NumMochilas; i++)
-            {
-                Console.WriteLine("Mochila " + (i + 1) + "\n" + populacao[i]);
-            }
+      //Metade 2
+      itensDisponiveis.Add(new Item(3, 6, "Moletom"));
+      itensDisponiveis.Add(new Item(5, 1, "Pedra"));
+      itensDisponiveis.Add(new Item(1, 6, "Relogio"));
+      itensDisponiveis.Add(new Item(2, 7, "ventilador"));
+      itensDisponiveis.Add(new Item(3, 5, "violao"));
+      itensDisponiveis.Sort((a, b) => a.Nome.CompareTo(b.Nome));
 
-            List<(Mochila moc, int inicio, int fim)> ranges = new List<(Mochila, int, int)>();
-            ranges.AddRange(RoletaRussa(populacao));
-            Random rand = new Random();
-            int rangeSorteado = rand.Next(1000000);
-            while (rangeSorteado > ranges[ranges.Count - 1].fim)
-            {
-                Console.WriteLine("AZAR DO CARAIO");
-                rangeSorteado = rand.Next(1000000);
-            }
-            var moc1 = ranges.Find(a => a.inicio <= rangeSorteado && a.fim >= rangeSorteado);
-            ranges.Remove(moc1);
-            ranges = new List<(Mochila, int, int)>();
-            ranges.AddRange(RoletaRussa(populacao, moc1.moc));
-            rangeSorteado = rand.Next(1000000);
-            while (rangeSorteado > ranges[ranges.Count - 1].fim)
-            {
-                Console.WriteLine("AZAR DO CARAIO");
-                rangeSorteado = rand.Next(1000000);
-            }
-            var moc2 = ranges.Find(a => a.inicio <= rangeSorteado && a.fim >= rangeSorteado);
+      for (int i = 0; i < NumMochilas; i++)
+      {
+        Mochila aux = new Mochila(CapacidadeMaxima, new List<Item>(itensDisponiveis));
+        populacao.Add(aux);
+        aptidaoMaxima += aux.Aptidations;
+      }
 
-            Console.WriteLine("GANHADORES:");
-            Console.WriteLine(moc1.moc);
-            Console.WriteLine(moc2.moc);
-        }
+      populacao.Sort((a, b) => b.Aptidations.CompareTo(a.Aptidations));
+      for (int i = 0; i < NumMochilas; i++)
+      {
+        Console.WriteLine("Mochila " + (i + 1) + "\n" + populacao[i]);
+      }
 
-        public static float getAptidaoMaxima(List<Mochila> mochilas)
-        {
-            float aptidaoMax = 0;
-            foreach (Mochila mochila in mochilas)
-            {
-                aptidaoMax += mochila.Aptidations;
-            }
-            return aptidaoMax;
-        }
-        public static float getAptidaoMaxima(List<Mochila> mochilas, Mochila moc)
-        {
-            float aptidaoMax = 0;
-            foreach (Mochila mochila in mochilas)
-            {
-                if (!mochila.Equals(moc))
-                    aptidaoMax += mochila.Aptidations;
-            }
-            return aptidaoMax;
-        }
+      List<(Mochila moc, int inicio, int fim)> ranges = new List<(Mochila, int, int)>();
+      ranges.AddRange(RoletaRussa(populacao));
+      Random rand = new Random();
+      int rangeSorteado = rand.Next(1000000);
+      while (rangeSorteado > ranges[ranges.Count - 1].fim)
+      {
+        Console.WriteLine("AZAR DO CARAIO");
+        rangeSorteado = rand.Next(1000000);
+      }
+      var moc1 = ranges.Find(a => a.inicio <= rangeSorteado && a.fim >= rangeSorteado);
+      ranges.Remove(moc1);
+      ranges = new List<(Mochila, int, int)>();
+      ranges.AddRange(RoletaRussa(populacao, moc1.moc));
+      rangeSorteado = rand.Next(1000000);
+      while (rangeSorteado > ranges[ranges.Count - 1].fim)
+      {
+        Console.WriteLine("AZAR DO CARAIO");
+        rangeSorteado = rand.Next(1000000);
+      }
+      var moc2 = ranges.Find(a => a.inicio <= rangeSorteado && a.fim >= rangeSorteado);
 
-        public static int calculaRange(float aptidao, float aptidaoMaxima)
-        {
-            return (int)((aptidao / aptidaoMaxima) * 1000000);
-        }
-
-        public static List<(Mochila, int, int)> RoletaRussa(List<Mochila> populacao, Mochila? mochila = null)
-        {
-            float aptidaoMaxima = 0;
-            if (mochila != null)
-            {
-                aptidaoMaxima = getAptidaoMaxima(populacao, mochila);
-            }
-            else
-            {
-                aptidaoMaxima = getAptidaoMaxima(populacao);
-            }
-            List<(Mochila moc, int inicio, int fim)> ranges = new List<(Mochila, int, int)>();
-            for (int j = 0, aux = 0; j < populacao.Count; j++)
-            {
-                if (!populacao[j].Equals(mochila))
-                {
-                    int range = calculaRange(populacao[j].Aptidations, aptidaoMaxima);
-                    if (j == 0 || aux == 0)
-                        ranges.Add((populacao[j], 0, range));
-                    else
-                        ranges.Add((populacao[j], ranges[aux - 1].fim + 1, ranges[aux - 1].fim + range));
-
-                    Console.WriteLine("[ " + aux + " ] - " + ranges[aux].inicio + " | | " + ranges[aux].fim);
-                    aux++;
-                }
-            }
-            return ranges;
-        }
+      Console.WriteLine("GANHADORES:");
+      Console.WriteLine(moc1.moc);
+      Console.WriteLine(moc2.moc);
+      Mochila f1, f2;
+      (f1, f2) = Cruzamento(moc1.moc, moc2.moc, new List<Item>(itensDisponiveis));
     }
+
+    public static float getAptidaoMaxima(List<Mochila> mochilas)
+    {
+      float aptidaoMax = 0;
+      foreach (Mochila mochila in mochilas)
+      {
+        aptidaoMax += mochila.Aptidations;
+      }
+      return aptidaoMax;
+    }
+    public static float getAptidaoMaxima(List<Mochila> mochilas, Mochila moc)
+    {
+      float aptidaoMax = 0;
+      foreach (Mochila mochila in mochilas)
+      {
+        if (!mochila.Equals(moc))
+          aptidaoMax += mochila.Aptidations;
+      }
+      return aptidaoMax;
+    }
+
+    public static int calculaRange(float aptidao, float aptidaoMaxima)
+    {
+      return (int)((aptidao / aptidaoMaxima) * 1000000);
+    }
+
+    public static List<(Mochila, int, int)> RoletaRussa(List<Mochila> populacao, Mochila? mochila = null)
+    {
+      float aptidaoMaxima = 0;
+      if (mochila != null)
+      {
+        aptidaoMaxima = getAptidaoMaxima(populacao, mochila);
+      }
+      else
+      {
+        aptidaoMaxima = getAptidaoMaxima(populacao);
+      }
+      List<(Mochila moc, int inicio, int fim)> ranges = new List<(Mochila, int, int)>();
+      for (int j = 0, aux = 0; j < populacao.Count; j++)
+      {
+        if (!populacao[j].Equals(mochila))
+        {
+          int range = calculaRange(populacao[j].Aptidations, aptidaoMaxima);
+          if (j == 0 || aux == 0)
+            ranges.Add((populacao[j], 0, range));
+          else
+            ranges.Add((populacao[j], ranges[aux - 1].fim + 1, ranges[aux - 1].fim + range));
+
+          Console.WriteLine("[ " + aux + " ] - " + ranges[aux].inicio + " | | " + ranges[aux].fim);
+          aux++;
+        }
+      }
+      return ranges;
+    }
+
+    //Cruzamento pai e mãe (mochilas)
+    public static (Mochila, Mochila) Cruzamento(Mochila m1, Mochila m2, List<Item> itensDisponiveis)
+    {
+      foreach (var item in m1.GetItems())
+      {
+        Console.WriteLine("item da 1ª mochila: " + item);
+      };
+      foreach (var item in m2.GetItems())
+      {
+        Console.WriteLine("itens da 2ª mochila: " + item);
+      };
+
+      //Criação do Filho 1
+      List<Item> listaFilho1 = new List<Item>();
+      for (int i = 0; i < (int)Math.Ceiling(itensDisponiveis.Count() / 2.0); i++)
+      {
+        if (m1.GetItems().Contains(itensDisponiveis[i]))
+          listaFilho1.Add(itensDisponiveis[i]);
+      }
+      for (int i = itensDisponiveis.Count() - 1; i >= (int)Math.Floor(itensDisponiveis.Count() / 2.0); i--)
+      {
+
+        if (m2.GetItems().Contains(itensDisponiveis[i]))
+          listaFilho1.Add(itensDisponiveis[i]);
+      }
+      Mochila filho1 = new Mochila(15, listaFilho1, true);
+
+      //Criação do Filho 2
+      List<Item> listaFilho2 = new List<Item>();
+      for (int i = 0; i < (int)Math.Ceiling(itensDisponiveis.Count() / 2.0); i++)
+      {
+        if (m2.GetItems().Contains(itensDisponiveis[i]))
+          listaFilho2.Add(itensDisponiveis[i]);
+      }
+      for (int i = itensDisponiveis.Count() - 1; i >= (int)Math.Floor(itensDisponiveis.Count() / 2.0); i--)
+      {
+        if (m1.GetItems().Contains(itensDisponiveis[i]))
+          listaFilho2.Add(itensDisponiveis[i]);
+      }
+      Mochila filho2 = new Mochila(15, listaFilho2, true);
+
+      //Exibição do filho 1
+      foreach (var item in filho1.GetItems())
+      {
+        Console.WriteLine("Itens do filho 1: " + item);
+      };
+      Console.WriteLine("Apitidão do filho 1: " + filho1.Aptidations);
+      //Exibição do filho 2
+      foreach (var item in filho2.GetItems())
+      {
+        Console.WriteLine("Itens do filho 2: " + item);
+      };
+      Console.WriteLine("Apitidão do filho 2: " + filho2.Aptidations);
+      return (filho1, filho2);
+    }
+  }
 }
